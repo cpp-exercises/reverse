79d30cdcec974b7b81aa9ffa1284ed8f00ed77a2
Erel Segal-Halevi <erelsgl@gmail.com>
2021-02-09 17:36:58 +0200

    Create Demo.cpp

---
+++ Demo.cpp
@@ -0,0 +1,13 @@
+/**
+ * A demo program for the "reverse" exercise.
+ * Author: Erel Segal-Halevi
+ * Since: 2021-02
+ */
+
+#include "reverse.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << reverse("abc") << endl;  // should print cba
+}
